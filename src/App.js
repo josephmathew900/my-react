c8394cd3e97dfc9968af09173662e958c4003fb2
josephmathew900 <josephmathew900@gmail.com>
2021-01-13 08:47:39 +0530

    class component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+/*
 function App() {
     return <div>Hello React</div>;
+}
+*/
+
+class App extends React.Component {
+    render(){
+        return <div>Hello React</div>;
+    }
 }
 
 export default App;

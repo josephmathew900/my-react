b630b6f490361d75ece63134d855e1a29642685a
josephmathew900 <josephmathew900@gmail.com>
2021-01-14 16:49:10 +0530

    Adding a new image

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,51 +1,43 @@
-import React, {useEffect} from 'react'
+import React, { useState } from 'react'
 
 
 export default function Images() {
 
-    useEffect(() => {
-        console.log("Images Component Mounted");
-        const interval =  setInterval(() => {
-            console.log("Hello");
-            },1000)
+    const [images, setimages] = useState([
+        "https://images.unsplash.com/photo-1610381812786-592c70146600?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1610492273249-6406a1154eae?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1610473068893-7a63129207d2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        "https://images.unsplash.com/photo-1610551001381-d62bbbbd9299?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+    ]);
 
-        return () => clearInterval(interval);
-    },[]);
+    function ShowImage(){
+        return images.map((image) => {
+            return (
+                <div>
+                    <img src={ image } width="150" alt="img ld"/>
+                </div>
+            )
+        })
+    }
+
+    function handleAdd(){
+        setimages([
+            ...images,
+            "https://images.unsplash.com/photo-1610507436343-9c5a9b8d8417?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80"
+        ]);
+    }
 
     return (
-        <img src="https://images.unsplash.com/photo-1610381812786-592c70146600?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" 
-        alt="img loading"/>
+        <section>
+            <div className="flex justify-center my-5">
+                <ShowImage />
+            </div>
+            <div className="flex justify-between">
+                <input type="text" className="p-2 border border-gray-800 shadow rounded"/>
+                <button className="p-2 bg-green-600 text-white" onClick={ handleAdd }>Add New</button>
+            </div>
+        </section>
     )
 }
 
 
-
-
-// export default class Images extends Component {
-
-//     constructor(props){
-//         super(props);
-//         this.state = {interval: null};
-//     }
-
-//     componentDidMount(){
-//         console.log("Images Component Mounted");
-
-//         this.setState({
-//             interval: setInterval(() => {
-//                 console.log('Hello');
-//         }, 1000)});
-//     }
-
-//     componentWillUnmount(){
-//         console.log("Images Component Unmounted");
-//         clearInterval(this.state.interval);
-//     }
-
-//     render() {
-//         return (
-//             <img src="https://images.unsplash.com/photo-1610381812786-592c70146600?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" 
-//             alt="img loading"/>
-//         );
-//     }
-// }

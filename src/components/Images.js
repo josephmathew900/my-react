561c29f7503fe58b92e7d58de98a3e1595ce637b
josephmathew900 <josephmathew900@gmail.com>
2021-01-14 17:28:30 +0530

    Adding a new image - using textbox

--- src/components/Images.js
+++ src/components/Images.js
@@ -10,6 +10,8 @@ export default function Images() {
         "https://images.unsplash.com/photo-1610551001381-d62bbbbd9299?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
     ]);
 
+    const [newImageUrl, setnewImageUrl] = useState("");
+
     function ShowImage(){
         return images.map((image) => {
             return (
@@ -21,19 +23,25 @@ export default function Images() {
     }
 
     function handleAdd(){
-        setimages([
-            ...images,
-            "https://images.unsplash.com/photo-1610507436343-9c5a9b8d8417?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80"
-        ]);
+        setimages([...images,newImageUrl]);
+        setnewImageUrl("");
+    }
+
+    function handleChange(event){
+        setnewImageUrl(event.target.value);
     }
 
     return (
         <section>
-            <div className="flex justify-center my-5">
+            <div className="flex flex-wrap justify-center my-5">
                 <ShowImage />
             </div>
             <div className="flex justify-between">
-                <input type="text" className="p-2 border border-gray-800 shadow rounded"/>
+                <input 
+                    type="text" 
+                    className="p-2 border border-gray-800 shadow rounded" 
+                    onChange={handleChange}
+                    value={ newImageUrl }/>
                 <button className="p-2 bg-green-600 text-white" onClick={ handleAdd }>Add New</button>
             </div>
         </section>

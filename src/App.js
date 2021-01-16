6322a89d9c141b30ddcd4d17d20008770fd0ab55
josephmathew900 <josephmathew900@gmail.com>
2021-01-16 12:50:17 +0530

    react-route-dom basic setups

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import "./assets/css/style.css";
 import Images from './components/Images';
 
-function App() {
-
+function Gallery() {
     return (
-            <section className="flex justify-center">
-                <div className="w-10/12">
-                    <div className="text-center">
-                        <Images />
-                    </div>
+        <section className="flex justify-center">
+            <div className="w-10/12">
+                <div className="text-center">
+                    <Images />
                 </div>
-            </section>
+            </div>
+        </section>
+    );
+}
+
+function Home() {
+    return(
+        <div className="flex h-screen">
+            <h1 className="m-auto text-3xl">Welcome Home</h1>
+        </div>
+    );
+}
+
+function Login() {
+    return(
+        <div className="flex h-screen">
+            <h1 className="m-auto text-3xl">Login Page</h1>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+
+                <Route exact path="/">
+                    <Home />
+                </Route>
+
+                <Route path="/login">
+                    <Login />
+                </Route>
+                
+                <Route path="/gallery">
+                    <Gallery />
+                </Route>
+
+            </Switch>
+        </Router>
+        
     );
 }
 

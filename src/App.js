630a74d2a9bb96f8a1af0efaa512ed50bc1fe2be
josephmathew900 <josephmathew900@gmail.com>
2021-01-16 16:00:23 +0530

    navbar basic setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./assets/css/style.css";
+import Header from "./components/Header";
 import routes from "./utils/routes";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         {routes.map((route) => (
           <Route

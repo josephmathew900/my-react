161d598adab0153e0855ae768a9301c37676ab14
josephmathew900 <josephmathew900@gmail.com>
2021-01-16 17:19:28 +0530

    login page design done

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAyocTiQ_QFwLad4-dZE-ziuMmwh-b8XrY",
+  authDomain: "in-depth-react-1-79fb1.firebaseapp.com",
+  projectId: "in-depth-react-1-79fb1",
+  storageBucket: "in-depth-react-1-79fb1.appspot.com",
+  messagingSenderId: "69524572424",
+  appId: "1:69524572424:web:26fdc00acd78556ed0c4bd",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
